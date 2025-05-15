424ace22168ecd57fc0fdeeef345b6c63e62dbfb
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-05-15 15:53:25 +0200

    Handle Errors/Exceptions, Implement Validation and Implement Custom Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
 using Application.Activities.Queries;
+using Application.Activities.Validators;
 using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using FluentValidation;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +13,17 @@ builder.Services.AddDbContext<AppDbContext> (opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
-builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddMediatR(x => {
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
+    x.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
+builder.Services.AddTransient<ExceptionMiddleware>();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router";
+import { ToastContainer } from "react-toastify";
 
 import { router } from "./app/router/Routes";
 import { store, StoreContext } from "./lib/store/store";
@@ -19,6 +21,11 @@ createRoot(document.getElementById("root")!).render(
     <StoreContext.Provider value={store}>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
+        <ToastContainer
+          position="bottom-right"
+          hideProgressBar
+          theme="colored"
+        />
         <RouterProvider router={router} />
       </QueryClientProvider>
     </StoreContext.Provider>
