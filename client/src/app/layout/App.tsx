1b3692158c2d84215c7b1f005ff6eb7c4011bcd3
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-05-26 18:18:38 +0200

    Implement the Attendance feature in the client app

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,19 +1,21 @@
-import { Box, Container } from "@mui/material";
-import { Outlet, useLocation } from "react-router";
+import { Box, Container, CssBaseline } from '@mui/material';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router';
 
-import NavBar from "./NavBar";
-import HomePage from "../../features/home/HomePage";
+import NavBar from './NavBar';
+import HomePage from '../../features/home/HomePage';
 
 function App() {
   const location = useLocation();
 
   return (
-    <Box sx={{ bgcolor: "#eee", minHeight: "100vh" }}>
-      {location.pathname === "/" ? (
+    <Box sx={{ bgcolor: '#eee', minHeight: '100vh' }}>
+      <ScrollRestoration />
+      <CssBaseline />
+      {location.pathname === '/' ? (
         <HomePage />
       ) : (
         <>
-          {" "}
+          {' '}
           <NavBar />
           <Container maxWidth="xl" sx={{ mt: 3 }}>
             <Outlet />
--- client/src/features/activities/details/ActivityDetailPage.tsx
+++ client/src/features/activities/details/ActivityDetailPage.tsx
@@ -1,10 +1,10 @@
-import { Grid, Typography } from "@mui/material";
-import { useParams } from "react-router";
-import { useActivities } from "../../../lib/hooks/useActivities";
-import ActivityDetailsHeader from "./ActivityDetailsHeader";
-import ActivityDetailsInfo from "./ActivityDetailsInfo";
-import ActivityDetailsChat from "./ActivityDetailsChat";
-import ActivityDetailsSidebar from "./ActivityDetailsSidebar";
+import { Grid, Typography } from '@mui/material';
+import { useParams } from 'react-router';
+import { useActivities } from '../../../lib/hooks/useActivities';
+import ActivityDetailsHeader from './ActivityDetailsHeader';
+import ActivityDetailsInfo from './ActivityDetailsInfo';
+import ActivityDetailsChat from './ActivityDetailsChat';
+import ActivityDetailsSidebar from './ActivityDetailsSidebar';
 
 export default function ActivityDetailPage() {
   const { id } = useParams();
@@ -22,7 +22,7 @@ export default function ActivityDetailPage() {
         <ActivityDetailsChat />
       </Grid>
       <Grid size={4}>
-        <ActivityDetailsSidebar />
+        <ActivityDetailsSidebar activity={activity} />
       </Grid>
     </Grid>
   );
