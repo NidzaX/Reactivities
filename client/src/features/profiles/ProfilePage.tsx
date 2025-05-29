b445b49a8f693a6ac5d3e3d372b79def262b1ba3
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-05-29 18:17:46 +0200

    Implement Client Image Uplaod

--- client/src/features/profiles/ProfilePage.tsx
+++ client/src/features/profiles/ProfilePage.tsx
@@ -1,13 +1,22 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import { useParams } from 'react-router';
 
 import ProfileHeader from './ProfileHeader';
 import ProfileContent from './ProfileContent';
 
+import { useProfile } from '../../lib/hooks/useProfile';
+
 export default function ProfilePage() {
+  const { id } = useParams();
+  const { profile, loadingProfile } = useProfile(id);
+
+  if (loadingProfile) return <Typography>Loading profile...</Typography>;
+
+  if (!profile) return <Typography>Porfile not found</Typography>;
   return (
     <Grid>
       <Grid size={12}>
-        <ProfileHeader />
+        <ProfileHeader profile={profile} />
         <ProfileContent />
       </Grid>
     </Grid>
